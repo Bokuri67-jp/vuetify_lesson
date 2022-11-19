e0d310b3578c82a524a217362afdb5380a0f1f9f
kubori <g.kbrt.prog@gmail.com>
2022-11-20 00:21:30 +0900

    email設定した。草生えるかな？

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <v-app>
+    <v-navigation-drawer app>Navigation Lists</v-navigation-drawer>
     <v-app-bar color="primary" dark app>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
       <v-toolbar-title>Vuetify</v-toolbar-title>
     </v-app-bar>
     <v-footer color="primary" dark app>
