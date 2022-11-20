10dd56960564b71ba0615ad79e1dc0a823cceeca
kubori <ry-kubo@rapide-act.com>
2022-11-20 13:04:06 +0900

    ボタン配置

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,16 @@
   <v-app>
     <v-navigation-drawer app v-model="drawer" clipped>Navigation Lists</v-navigation-drawer>
     <v-app-bar color="primary" dark app clipped-left>
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon @click="drawer=!drawer"></v-app-bar-nav-icon>
       <v-toolbar-title>Vuetify</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+      <v-btn text>FOR ENTERPRISE</v-btn>
+      <v-btn text>SUPPORT</v-btn>
+      </v-toolbar-items>
     </v-app-bar>
+    <v-main>
+    </v-main>
     <v-footer color="primary" dark app>
       Vuetify
     </v-footer>
