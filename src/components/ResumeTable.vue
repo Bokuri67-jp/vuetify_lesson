5f6ce2d190db03cb204be59d4026ab7e1e6c979c
kubori <ry-kubo@rapide-act.com>
2022-12-07 23:25:49 +0900

    ちょっと整えた

--- src/components/ResumeTable.vue
+++ src/components/ResumeTable.vue
@@ -17,29 +17,90 @@
           </v-card>
         </v-col>
     </v-row>
+    <br>
 
-    <v-col>
-      <v-data-table
-      :headers="headers"
-      :items="desserts"
-      :items-per-page="5"
-      class="elevation-1"
-      ></v-data-table>
-    </v-col>
+    <v-data-table
+    :headers="resumeSummaryHeaders"
+    :items="resumeSummary"
+    :items-per-page="5"
+    class="elevation-1"
+    ></v-data-table>
+    <br>
+
+    <v-data-table
+    :headers="resumeHeaders"
+    :items="resume"
+    :items-per-page="5"
+    class="elevation-1"
+    ></v-data-table>
   </v-container>
 
 </template>
 
 <script>
-// import Desserts from './Desserts'
 
 export default {
-  // components: { Desserts },
   data () {
     return {
-      desserts: [
+      resumeSummaryHeaders: [
+        {
+          text: '入社年月 - 退社年月',
+          align: 'start',
+          sortable: false,
+          value: 'name'
+        },
+        { text: '在籍期間', value: 'calories' },
+        { text: '業種', value: 'fat' },
+        { text: '会社名', value: 'carbs' }
+      ],
+      resumeSummary: [
         {
           name: 'Frozen Yogurt',
+          calories: 159,
+          fat: 6.0,
+          carbs: 24,
+          protein: 4.0,
+          iron: '1%'
+        },
+        {
+          name: 'Ice cream sandwich',
+          calories: 237,
+          fat: 9.0,
+          carbs: 37,
+          protein: 4.3,
+          iron: '1%'
+        },
+        {
+          name: 'Eclair',
+          calories: 262,
+          fat: 16.0,
+          carbs: 23,
+          protein: 6.0,
+          iron: '7%'
+        },
+        {
+          name: 'Cupcake',
+          calories: 305,
+          fat: 3.7,
+          carbs: 67,
+          protein: 4.3,
+          iron: '8%'
+        }
+      ],
+      resumeHeaders: [
+        {
+          text: '入社年月 - 退社年月',
+          align: 'start',
+          sortable: false,
+          value: 'name'
+        },
+        { text: '在籍期間', value: 'calories' },
+        { text: '業種', value: 'fat' },
+        { text: '会社名', value: 'carbs' }
+      ],
+      resume: [
+        {
+          name: 'ああああああああ',
           calories: 159,
           fat: 6.0,
           carbs: 24,
